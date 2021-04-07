c1f0a3354d2812ae286f669cce045023d2885ee4
Sarah <sara616b@stud.kea.dk>
2021-04-07 15:15:18 +0200

    front page mobile first edition-ish

---
+++ assets/scripts/menu.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  document.querySelector("#burgermenu").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+  console.log("toggleMenu");
+  document.querySelector("#burgerhidden").classList.toggle("hidden");
+
+  document.querySelector("#burgermenu").innerHTML = "=";
+
+  if (document.querySelector("#burgerhidden").classList.contains("hidden")) {
+    document.querySelector("#burgermenu").innerHTML = "=";
+  } else {
+    document.querySelector("#burgermenu").innerHTML = "X";
+  }
+}
